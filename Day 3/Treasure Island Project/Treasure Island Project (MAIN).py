e7d5f1cb394b73d0b2fced1cac552d6605eeade3
Aniket <aniketshrivastava12345@gmail.com>
2025-11-04 13:22:10 +0530

    initial

---
+++ Day 3/Treasure Island Project/Treasure Island Project (MAIN).py
@@ -0,0 +1,52 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+player = input('''You're at a cross road. Where do you wanna go??
+                   Type 'Left' or 'Right'?\n''').lower()
+
+if player == "left":
+   print("Congratulations!! You've successfully passed the first stage!")
+   Choice_1 = input('''You see an island right in the middle of the lake!!
+You're stuck with two options again!
+                    Type 'Swim' or 'Wait' as per your desired choice!\n''').lower()
+   if Choice_1 == "wait":
+       print("Congratulations!! You're another step closer from discovering your treasure!!")
+       Choice_2 = input('''After crossing the lake through a boat. 
+Now, you came across multiple doors in the chamber!!
+Each chamber door holds a secret, but you shortlists only 3 of them.
+                         Type 'Yellow' or 'Blue' or 'Red' to select either of the door...\n''').lower()
+       if Choice_2 == "yellow":
+           print("OMG!! You've found your treasure!! You're a freaking MILLIONAIRE now!!!🎉🎉")
+       elif Choice_2 == "red":
+           print("You got burnt by fire🔥!! GAME OVER 😭")
+       elif Choice_2 == "blue":
+           print("Oh shit!! You've been eaten by furious Cannibals🧟!! GAME OVER 😭")
+       else:
+           print("You've been discovered by Pirates🏴‍☠️!! GAME OVER 😭")
+   elif Choice_1 == "swim":
+       print("You've been eaten up by Alligators🐊!! GAME OVER😭")
+   else:
+       print("You drowned in the deep lake!! GAME OVER😭")
+else:
+    print("You fell into a deep hole!! Game Over😭")
